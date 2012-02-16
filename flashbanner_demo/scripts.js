c7f351bf7a59f3c6fff6a56daf5a1909b0f9a706
Jack Armley <jack_armley@yahoo.co.uk>
2012-02-16 07:23:39 +0000

    changes

--- flashbanner_demo/scripts.js
+++ flashbanner_demo/scripts.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
-	var flashversion = "9.0.0"
-	var bannercontainer = "banner"
-	var bannerurl = "";
-	var dimensions = ""
-	$("li a").each(function(){
-		$(this).click(function(){
-			bannerurl = $(this).attr("data-url");
-			dimensions = $(this).attr("class").split(/\s+/);
-			swfobject.embedSWF(bannerurl, bannercontainer, dimensions[0], dimensions[1], flashversion);
-		});
-	});
+$(document).ready(function(){ 
+	//Set flash player required version and banner container ID
+		var flashversion = "9.0.0"
+		var bannercontainer = "banner"      
+	//Set the goodies for each banner 	
+		var bannerurl = "";
+		var bannerdimensions = "";  
+		var bannertitle = "";
+	//Run the banner swap function	
+		$("select").change(function () {
+			$("select option:selected").each(function () {
+				bannerurl = $(this).attr("value");
+				bannerdimensions = $(this).attr("class").split(/\s+/);   
+				bannertitle = $(this).text();
+				
+				swfobject.embedSWF(bannerurl, bannercontainer, bannerdimensions[0], bannerdimensions[1], flashversion); 
+				$("section h1").text(bannertitle); 
+				$("#banner_wrapper").width(bannerdimensions[0]);
+			});
+		})
+		.change();
 });
