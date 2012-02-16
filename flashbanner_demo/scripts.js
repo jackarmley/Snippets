65f953a1285fc25405017589b0c5e706753e3cec
unknown <Jack Armley@.(none)>
2012-02-16 06:19:39 +0000

    Amends

--- flashbanner_demo/scripts.js
+++ flashbanner_demo/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	var flashversion = "9.0.0"
 	var bannercontainer = "banner"
-	$("#banner_1").click(function(){
-		swfobject.embedSWF("http://files.bannersnack.net/site/examples/31.swf", bannercontainer, "250", "250", flashversion);
-	});
-	$("#banner_2").click(function(){
-		swfobject.embedSWF("http://files.bannersnack.net/site/examples/37.swf", bannercontainer, "336", "280", flashversion);
-	});
-	$("#banner_3").click(function(){
-		swfobject.embedSWF("http://files.bannersnack.net/site/examples/35.swf", bannercontainer, "160", "600", flashversion);
+	var bannerurl = "";
+	var dimensions = ""
+	$("li a").each(function(){
+		$(this).click(function(){
+			bannerurl = $(this).attr("data-url");
+			dimensions = $(this).attr("class").split(/\s+/);
+			swfobject.embedSWF(bannerurl, bannercontainer, dimensions[0], dimensions[1], flashversion);
+		});
 	});
 });
